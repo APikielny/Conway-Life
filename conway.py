172a90dbe80fd6c120e32e27a2be661fd0f8efbc
APikielny <apikielny@gmail.com>
2015-10-15 14:47:09 -0400

    Updated from Brython Server: October 15, 2015 2:47:08 PM EDT

--- conway.py
+++ conway.py
@@ -18,6 +18,17 @@ noline = LineStyle(0, black)
 bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, noline, black)
 bg = Sprite(bg_asset, (0,0))
 
+class Conway(App):
+    """
+    Tutorial4 space game example.
+    """
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        black = Color(0, 1)
+        noline = LineStyle(0, black)
+        bg_asset = RectangleAsset(width, height, noline, black)
+        bg = Sprite(bg_asset, (0,0))
 
-myapp = App(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+myapp = Conway(SCREEN_WIDTH, SCREEN_HEIGHT)
 myapp.run()
