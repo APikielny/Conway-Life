7f905c30a6023f2222fd89b1b0a62941262ac6a8
APikielny <apikielny@gmail.com>
2015-10-15 14:54:41 -0400

    Updated from Brython Server: October 15, 2015 2:54:40 PM EDT

--- conway.py
+++ conway.py
@@ -16,8 +16,6 @@ SCREEN_HEIGHT = 480
 # Background
 black = Color(0, 1)
 noline = LineStyle(0, black)
-bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, noline, black)
-bg = Sprite(bg_asset, (0,0))
 
 class Conway(App):
     """
