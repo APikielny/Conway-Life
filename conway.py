157cb30b9318b2680c25cbd66088fcd3d3c6ec0e
APikielny <apikielny@gmail.com>
2015-10-15 14:42:36 -0400

    Updated from Brython Server: October 15, 2015 2:42:36 PM EDT

--- conway.py
+++ conway.py
@@ -6,3 +6,8 @@ Assignment:
 Write and submit a program that plays Conway's Game of Life, per 
 https://github.com/HHS-IntroProgramming/Conway-Life
 """
+from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
+
+#need to decide on boundaries: wrap or nah
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
