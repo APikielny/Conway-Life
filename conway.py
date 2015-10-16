e7d6c28fa65348e125ea7729c06f88a864fa0d19
APikielny <apikielny@gmail.com>
2015-10-16 13:40:53 -0400

    Updated from Brython Server: October 16, 2015 1:40:53 PM EDT

--- conway.py
+++ conway.py
@@ -20,10 +20,14 @@ noline = LineStyle(0, black)
 class Conway(App):
     def __init__(self, width, height):
         super().__init__(width, height)
+        #initiate with white?
         Cell((100,100))
         Cell((200,100))
         Cell((300,100))
         Cell((100,200))
+     
+    def initiate("""change the attributes"""self, event):
+        self.color = black
         
 class Cell(Sprite):
     pixel = RectangleAsset(5,5,noline,black)
@@ -31,8 +35,6 @@ class Cell(Sprite):
     def __init__(self, position):
         super().__init__(Cell.pixel, position)
         Conway.listenKeyEvent("keydown", "space", self.#initiate cell)
-
-
 
 myapp = Conway(SCREEN_WIDTH, SCREEN_HEIGHT)
 myapp.run()
